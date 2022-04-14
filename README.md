c72897afe0e27f3ee4667516018766255f82925f
bharathpbk <bharath.parlapalli@codeitinc.com>
2022-04-14 21:59:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-i38afu-dqj72w
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-i38afu-dqj72w)
---
+++ src/app/accordion-header.ts
@@ -0,0 +1,18 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'ngbd-accordion-header',
+  templateUrl: './accordion-header.html',
+  encapsulation: ViewEncapsulation.None,
+  styles: [`
+    .card.disabled {
+      opacity: 0.5;
+    }
+    .custom-header::after {
+      content: none;
+    }
+  `]
+})
+export class NgbdAccordionHeader {
+  disabled = false;
+}
